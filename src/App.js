610def8d5837c18b1644c8f1eb83cc14b6310124
nome <nome>
2023-04-20 15:37:12 -0300

    terceiro commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Segment } from 'semantic-ui-react';
 import './App.css';
-import FormProduto from './views/produto/FormProduto';
-
+import MenuSistema from "./MenuSistema";
+import Rotas from './Rotas';
 function App() {
   return (
     <div className="App">
-     <FormProduto />
+
+      <MenuSistema />
+        
+      <Rotas />
 
       <div style={{marginTop: '6%'}}>
         <Segment vertical color='grey' size='tiny' textAlign='center'>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import './index.css';
@@ -9,8 +10,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
